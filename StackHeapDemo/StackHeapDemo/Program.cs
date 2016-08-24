2ec748b60c79acdf8a9bf7c247f7c41eb19135e7
Johnson Ho <m97_jno@hotmail.com>
2016-08-24 11:19:07 +0200

    demo

---
+++ StackHeapDemo/StackHeapDemo/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StackHeapDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1 = new Person("Bo", 30);
+            CelebrateBirthday(p1);
+            Console.WriteLine(p1.Age);
+            p1 = null;
+
+            int i = 10;
+            Increase(i);
+            Console.WriteLine(i);
+
+        }
+
+        static void CelebrateBirthday(Person person)
+        {
+            person.Age++;
+        }
+
+        static void Increase(int value)
+        {
+            value++;
+        }
+    }
+
+    // Ref-type
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
