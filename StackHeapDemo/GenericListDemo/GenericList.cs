42d2476a9f7b032a17b7159a88fb3f5f6ab09a75
Johnson Ho <m97_jno@hotmail.com>
2016-08-24 13:00:11 +0200

    update in GenericListDemo

---
+++ StackHeapDemo/GenericListDemo/GenericList.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericListDemo
+{
+    public class GenericList<T> : IEnumerable<T>
+    {
+        public IEnumerator<T> GetEnumerator()
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                yield return array[i];
+            }
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return (IEnumerator) this;
+        }
+
+        public GenericList()
+        {
+            array = new T[0];
+        }
+
+        public void Add(T value)
+        {
+            var tempArray = new T[Count + 1];
+            for (int i = 0; i < Count; i++)
+            {
+                tempArray[i] = array[i];
+            }
+            tempArray[Count] = value;
+            array = tempArray;
+        }
+
+
+        public T this[int index]
+        {
+            get
+            {
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException();
+                else
+                {
+                    return (T)array[index];
+                }
+            }
+            set
+            {
+                if (index < 0 || index >= Count)
+                    throw new ArgumentOutOfRangeException();
+                else
+                {
+                    array[index] = value;
+                }
+            }
+        }
+
+        public int Count {
+            get
+            {
+                if (array == null)
+                    throw new NullReferenceException("internal array has not instantiated.");
+                else
+                {
+                    return array.Length;
+                }
+            }
+        }
+
+        private T[] array;
+    }
+}
---
+++ StackHeapDemo/GenericListDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericListDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var intList = new GenericList<int>();
+            intList.Add(4);
+            Console.WriteLine(intList.Count);
+            intList.Add(5);
+            Console.WriteLine(intList.Count);
+            intList.Add(6);
+            Console.WriteLine(intList.Count);
+
+            
+            for (int i = 0; i < intList.Count; i++)
+            {
+                Console.WriteLine(intList[i]);
+            }
+
+            var pList = new GenericList<Person>();
+            pList.Add(new Person("Bo", 13));
+            pList.Add(new Person("Doe", 40));
+            pList.Add(new Person("Allan", 55));
+
+            for (int i = 0; i < pList.Count; i++)
+            {
+                Console.WriteLine(pList[i].Name + " " +pList[i].Age);
+            }
+
+            foreach (var p in pList)
+            {
+                Console.WriteLine(p.Name + " " + p.Age);
+            }
+        }
+    }
+
+    class Person
+    {
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
