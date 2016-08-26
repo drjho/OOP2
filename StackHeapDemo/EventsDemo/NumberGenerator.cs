62e25c1f3228559f7db6d8db9154b0a2610aaf09
Johnson Ho <m97_jno@hotmail.com>
2016-08-26 21:40:10 +0200

    updated EventsDemo, revised naming of variables. removed duplicated project(sln)

---
+++ StackHeapDemo/EventsDemo/NumberGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDemo
+{
+    public class NumberGenerator
+    {
+        public delegate void EventHandler(object sender, EvenEventArgs e);
+
+        public event EventHandler EvenNumberEvent;
+
+        Random random = new Random();
+
+        public void Start()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                int randNum = random.Next();
+                if (randNum % 2 == 0)
+                {
+                    EvenNumberEvent?.Invoke(this, new EvenEventArgs(randNum));
+                }
+            }
+        }
+    }
+
+    public class EvenEventArgs : EventArgs
+    {
+        public EvenEventArgs(int i)
+        {
+            number = i;
+        }
+
+        public int number { get; private set; }
+    }
+
+}
---
+++ StackHeapDemo/EventsDemo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventsDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var numGen = new NumberGenerator();
+            numGen.EvenNumberEvent += OnEvenNumberEvent;
+            
+            numGen.Start();
+        }
+
+        static void OnEvenNumberEvent(object sender, EvenEventArgs e)
+        {
+            Console.WriteLine("Jämt tal: " + e.number);
+        }
+    }
+}
